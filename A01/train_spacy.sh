44796b93507613bf0f0789296b79f4bf538b04c2
John Stein <johndouglasstein@gmail.com>
2020-03-23 09:25:11 -0400

    updated spacy script

--- A01/train_spacy.sh
+++ A01/train_spacy.sh
@@ -10,6 +10,7 @@ then
 	echo "Please supply number of training cycles as argument";
 fi
 
+
 # command to train spacy models on the split all.conll data
 # user must specify number of training cycles
 python -m spacy train en spacymodel "${datadir}"all.train.json "${datadir}"all.dev.json -n $1;
